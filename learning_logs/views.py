98515d6e0baabf8224789406cc0754825ef10009
learning_log <ranxuhui33@163.com>
2018-05-06 23:03:27 +0800

    start captain 12

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -26,8 +26,9 @@ def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic = Topic.objects.get(id=topic_id)
     # 请确认请求的主题属于当前用户
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
+    # if topic.owner != request.user:
+    #     raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -57,7 +58,7 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """在特定的主题中添加新条目"""
     topic = Topic.objects.get(id=topic_id)
-
+    check_topic_owner(request, topic)
     if request.method != 'POST':
         # 未提交数据，创建一个空表单
         form = EntryForm()
@@ -79,8 +80,9 @@ def edit_entry(request, entry_id):
     """编辑既有条目"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
+    # if topic.owner != request.user:
+    #     raise Http404
 
     if request.method != 'POST':
         # 初次请求，使用当前条目填充表单
@@ -95,3 +97,8 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+
+def check_topic_owner(request, topic):
+    if topic.owner != request.user:
+        raise Http404
